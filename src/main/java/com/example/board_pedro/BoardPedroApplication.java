17942fd582709f16a52fd6ccebc53250f3d406d1
hyujikoh <42793489+hyujikoh@users.noreply.github.com>
2022-04-30 15:23:58 +0900

    Initial commit

---
+++ src/main/java/com/example/board_pedro/BoardPedroApplication.java
@@ -0,0 +1,13 @@
+package com.example.board_pedro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardPedroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardPedroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/board_pedro/BoardPedroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.board_pedro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardPedroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
